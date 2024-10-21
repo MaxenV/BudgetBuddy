f15a8340cd68e1a2f40db96303c03af3e9712958
MaxenV <maxen2001@gmail.com>
2024-10-21 23:03:17 +0200

    Generate private key for JwtService

--- budget_buddy/src/main/java/com/example/budget_buddy/service/JwtService.java
+++ budget_buddy/src/main/java/com/example/budget_buddy/service/JwtService.java
@@ -1,27 +1,31 @@
 package com.example.budget_buddy.service;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.SignatureException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Date;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
 
 @Service
 public class JwtService {
 
-    private final String secretKey = "your_secret_key";
+    private final String secretKey = JwtService.generateKey();
 
     public String generateToken(String email) {
-        return Jwts.builder()
+        var token = Jwts.builder()
             .setSubject(email) // Generate token with email
             .setIssuedAt(new Date())
             .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // Token valid for 10 hours
             .signWith(SignatureAlgorithm.HS256, secretKey)
             .compact();
+        return token;
     }
 
     public String extractUsername(String token) {
@@ -49,5 +53,13 @@ public class JwtService {
         } catch (SignatureException e) {
             return true; // Token signature is invalid
         }
+    }
+
+    private static String generateKey(){
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] key = new byte[32]; // 256 bits
+        secureRandom.nextBytes(key);
+        String base64Key = Base64.getEncoder().encodeToString(key);
+        return base64Key;
     }
 }
