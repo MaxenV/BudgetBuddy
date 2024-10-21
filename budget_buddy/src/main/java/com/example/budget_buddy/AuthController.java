5d5deb754b9a137caf734f067287e53830c516e8
MaxenV <maxen2001@gmail.com>
2024-10-21 22:50:47 +0200

    Fill registration to work properly

--- budget_buddy/src/main/java/com/example/budget_buddy/AuthController.java
+++ budget_buddy/src/main/java/com/example/budget_buddy/AuthController.java
@@ -1,11 +1,11 @@
 package com.example.budget_buddy;
 
+import java.util.Optional;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.budget_buddy.dto.LoginRequest;
 import com.example.budget_buddy.dto.RegisterRequest;
 import com.example.budget_buddy.service.JwtService;
-import com.example.budget_buddy.UserRepository;
-import java.util.Optional;
 
 
 @RestController
@@ -52,10 +50,17 @@ public class AuthController {
 
 
     @PostMapping("/register")
-public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
-    String encodedPassword = passwordEncoder().encode(registerRequest.getPassword());
-    // Zapisz użytkownika z zakodowanym hasłem do bazy danych
-    return ResponseEntity.ok("User registered successfully");
-}
+    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+        String encodedPassword = passwordEncoder().encode(registerRequest.getPassword());
+        User user = new User();
+        user.setName(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodedPassword);
+        user.setStatus("user");
+        user.setMessage(registerRequest.getMessage());
+        userRepository.save(user);
+        
+        return ResponseEntity.ok("User registered successfully");
+    }
 
 }
