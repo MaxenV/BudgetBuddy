245028cacdaf409dc5afc7edc3604251513d58a8
MaxenV <maxen2001@gmail.com>
2024-10-23 21:25:06 +0200

    Encrypt user password

--- budget_buddy/src/main/java/com/example/budget_buddy/service/AuthenticationService.java
+++ budget_buddy/src/main/java/com/example/budget_buddy/service/AuthenticationService.java
@@ -1,12 +1,13 @@
 package com.example.budget_buddy.service;
-import com.example.budget_buddy.dto.RegisterUserDto;
-import com.example.budget_buddy.dto.LoginUserDto;
-import org.springframework.stereotype.Service;
-import com.example.budget_buddy.User;
-import com.example.budget_buddy.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.budget_buddy.User;
+import com.example.budget_buddy.UserRepository;
+import com.example.budget_buddy.dto.LoginUserDto;
+import com.example.budget_buddy.dto.RegisterUserDto;
 
 
 @Service
@@ -31,7 +32,7 @@ public class AuthenticationService {
         User user = new User();
         user.setFullName(input.getFullName());
         user.setEmail(input.getEmail());
-        user.setPassword(input.getPassword());        
+        user.setPassword(passwordEncoder.encode(input.getPassword()));        
 
         return userRepository.save(user);
     }
