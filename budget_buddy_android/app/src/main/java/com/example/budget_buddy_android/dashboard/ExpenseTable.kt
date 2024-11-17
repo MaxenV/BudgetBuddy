54354f559b057e5b9d94f3e4e334bb41dec7d929
MaxenV <maxen2001@gmail.com>
2024-11-17 14:18:50 +0100

    Expense table ui
    Wrap long Expense Names

--- budget_buddy_android/app/src/main/java/com/example/budget_buddy_android/dashboard/ExpenseTable.kt
+++ budget_buddy_android/app/src/main/java/com/example/budget_buddy_android/dashboard/ExpenseTable.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.budget_buddy_android.models.Expense
@@ -18,23 +21,56 @@ fun ExpenseTable(expenses: List<Expense>) {
     Column {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.padding(8.dp).fillMaxWidth()
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
         ) {
-            Text("Expense Name")
-            Text("Coast")
-            Text("Category")
+            Text(
+                "Expense Name",
+                modifier = Modifier
+                    .width(150.dp)
+                    .wrapContentWidth(Alignment.Start)
+            )
+            Text(
+                "Coast",
+                modifier = Modifier
+                    .width(100.dp)
+                    .wrapContentWidth(Alignment.CenterHorizontally)
+            )
+            Text("Category", modifier = Modifier
+                .width(100.dp)
+                .wrapContentWidth(Alignment.End))
         }
         LazyColumn(
-            modifier = Modifier.padding(8.dp).fillMaxWidth()
-        )  {
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
             items(expenses) { expense ->
                 Row(
                     horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier.padding(8.dp).fillMaxWidth()
+                    modifier = Modifier
+                        .padding(8.dp, 15.dp)
+                        .fillMaxWidth()
                 ) {
-                    Text(expense.expenseName)
-                    Text(expense.coast.toString())
-                    Text(expense.category)
+                    Text(
+                        expense.expenseName,
+                        modifier = Modifier
+                            .width(150.dp)
+                            .wrapContentWidth(Alignment.Start)
+                    )
+                    Text(
+                        expense.coast.toString(),
+                        modifier = Modifier
+                            .width(100.dp)
+                            .wrapContentWidth(Alignment.CenterHorizontally)
+                    )
+                    Text(
+                        expense.category,
+                        modifier = Modifier
+                            .width(100.dp)
+                            .wrapContentWidth(Alignment.End)
+                    )
                 }
             }
         }
