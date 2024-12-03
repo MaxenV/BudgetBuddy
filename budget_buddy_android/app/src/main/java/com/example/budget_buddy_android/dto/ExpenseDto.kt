874b16eba97890352539f65bd888ebcf015f4eb3
MaxenV <maxen2001@gmail.com>
2024-12-03 15:26:51 +0100

    Change ExpenseDto from java to kotlin

--- budget_buddy_android/app/src/main/java/com/example/budget_buddy_android/dto/ExpenseDto.kt
+++ budget_buddy_android/app/src/main/java/com/example/budget_buddy_android/dto/ExpenseDto.kt
@@ -1,26 +1,32 @@
-package com.example.budget_buddy_android.dto;
+package com.example.budget_buddy_android.dto
 
-import com.example.budget_buddy_android.models.Expense;
+import com.example.budget_buddy_android.models.Expense
+import java.math.BigDecimal
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
-public class ExpenseDto {
-    private int id;
-    private String expenseName;
-    private BigDecimal cost;
-    private String category;
-    private String description;
-    private String expenseDateTime;
-
-    public ExpenseDto(Expense expense) {
-        this.id = expense.getId();
-        this.expenseName = expense.getExpenseName();
-        this.cost = expense.getCost();
-        this.category = expense.getCategory();
-        this.description = expense.getDescription();
-        this.expenseDateTime = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.getDefault()).format(expense.getExpenseDateTime());
-    }
-
+data class ExpenseDto(
+    val expenseName: String,
+    val cost: BigDecimal,
+    val category: String,
+    val description: String,
+    val expenseDateTime: String
+) {
+    constructor(expense: Expense) : this(
+        expenseName = expense.expenseName,
+        cost = expense.cost,
+        category = expense.category,
+        description = expense.description,
+        expenseDateTime = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.getDefault()).format(expense.expenseDateTime)
+    )
+    constructor():this(
+        expenseName = "",
+        cost = BigDecimal(0),
+        category = "",
+        description = "",
+       expenseDateTime = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.getDefault()).format(
+           Date()
+       )
+    )
 }
